2ebe35db011db33db1821d0f153ebbbd4ba28f0f
5h15h1r <shishir.agrahari14@gmail.com>
2024-08-23 17:13:26 +0530

    fix: added image links in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   images: {
     // domains: ["vaave.s3.amazonaws.com"],
-    remotePatterns: [{ protocol: "https", hostname: "images.pexels.com" }],
+    remotePatterns: [{ protocol: "https", hostname: "images.pexels.com" }, { protocol: "https", hostname: "psgroup.in" },{ protocol: "https", hostname: "alexandro.in" }],
   },
 };
 
